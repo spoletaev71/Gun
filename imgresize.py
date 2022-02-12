5597f248351a5ea8dd94cb4c3c474113e69288c8
Sergey Poletaev <s_poletaev@mail.ru>
2022-02-12 20:02:39 +0500

    vscode test

---
+++ imgresize.py
@@ -0,0 +1,36 @@
+from PIL import Image
+
+
+def scale_image(input_image_path,
+                output_image_path,
+                width=None,
+                height=None
+                ):
+    original_image = Image.open(input_image_path)
+    w, h = original_image.size
+    print('The original image size is {wide} wide x {height} '
+          'high'.format(wide=w, height=h))
+
+    if width and height:
+        max_size = (width, height)
+    elif width:
+        max_size = (width, h)
+    elif height:
+        max_size = (w, height)
+    else:
+        # No width or height specified
+        raise RuntimeError('Width or height required!')
+
+    original_image.thumbnail(max_size, Image.ANTIALIAS)
+    original_image.save(output_image_path)
+
+    scaled_image = Image.open(output_image_path)
+    width, height = scaled_image.size
+    print('The scaled image size is {wide} wide x {height} '
+          'high'.format(wide=width, height=height))
+
+
+if __name__ == '__main__':
+    scale_image(input_image_path='ScreenBG01.jpg',
+                output_image_path='ScreenBG01_new.jpg',
+                width=800)
